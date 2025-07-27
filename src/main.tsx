b0a7365b6be029efa466661a5eace2ec107ce084
Skrill Khan <skrill00010@gmail.com>
2025-07-27 18:45:40 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container missing in index.html');
+createRoot(container).render(<App />);
